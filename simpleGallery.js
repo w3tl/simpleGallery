16d385a5582510fe62ed51b9a712a4d851a2ba50
Vitaly <dostovalovvs@gmail.com>
2013-12-12 10:59:00 +1200

    v0.9.2
    + loading animation
    * code optimization

--- simpleGallery.js
+++ simpleGallery.js
@@ -1,40 +1,49 @@
 /*
- * simpleGallery v0.9.1
+ * simpleGallery v0.9.2
  * https://github.com/w3tl/simpleGallery
  *
  * Copyright 2013 Vitaly Dostovalov (dostovalovvs@gmail.com)
  * Released under the MIT license.
  *
- * Date: 2013.12.11
+ * Date: 2013.12.12
  */
 (function ($) {
     $.fn.gallery = function () {
         this.prepend(
-            '<div id="view"><span id="prev" class="control"></span><span id="close" class="control"></span><span id="next" class="control"></span><img></div>'
+            '<div id="view"><span id="prev" class="control"></span><span id="close" class="control"></span><span id="next" class="control"></span><img><div id="bubbl"><span id="bubbl1"></span><span id="bubbl2"></span><span id="bubbl3"></span></div></div>'
         );
-
         var view = $("#view"),
             view_image = view.children("img"),
+            loading = $("#bubbl"),
             curr_image;
 
         function viewImage() {
             full_image = curr_image.data("full");
-            view_image.attr('src', full_image);
+            loading.show();
+            view_image.hide().attr('src', full_image).load(function() {
+                loading.hide();
+                view_image.show();
+            });
         };
-        this.find("img").click(function () {
-            curr_image = $(this);
-            viewImage();
-            view.show();
+        
+        this.click(function(e) {
+            var clicked = $(e.target)[0].tagName === "IMG" ? $(e.target) : null ;
+            if ( clicked ) {
+                curr_image = clicked;
+                view.show();
+                viewImage();  
+            }
         });
-        view.find("#close").click(function () {
-            view.hide();
+        
+        $( "#close", view ).click(function () { 
+            view.hide(); 
         });
-        view.find("#prev").click(function () {
+        $( "#prev", view ).click(function () {
             curr_image = curr_image.prev("img").length ?
                 curr_image.prev() : curr_image.nextAll().last();
             viewImage();
         });
-        view.find("#next").click(function () {
+        $( "#next", view ).click(function () {
             curr_image = curr_image.next().length ?
                 curr_image.next() : curr_image.siblings("img").first();
             viewImage();
