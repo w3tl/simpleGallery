c2b79530961635922ddbb73cfb8e9b2f2a66fce9
Dostovalov Vitaly <dostovalovvs@gmail.com>
2013-12-14 19:01:18 +1200

    v0.9.3

--- js/simpleGallery.js
+++ js/simpleGallery.js
@@ -1,43 +1,59 @@
 /*
- * simpleGallery v0.9.1
+ * simpleGallery v0.9.3
  * https://github.com/w3tl/simpleGallery
  *
  * Copyright 2013 Vitaly Dostovalov (dostovalovvs@gmail.com)
  * Released under the MIT license.
  *
- * Date: 2013.12.11
+ * Date: 2013.12.13
  */
 (function ($) {
     $.fn.gallery = function () {
-        this.prepend(
-            '<div id="view"><span id="prev" class="control"></span><span id="close" class="control"></span><span id="next" class="control"></span><img></div>'
-        );
-
-        var view = $("#view"),
-            view_image = view.children("img"),
-            curr_image;
-
-        function viewImage() {
-            full_image = curr_image.data("full");
-            view_image.attr('src', full_image);
-        };
-        this.find("img").click(function () {
-            curr_image = $(this);
-            viewImage();
-            view.show();
+        return this.each(function() {
+            var gallery = $(this),
+                view = $("#view", gallery),
+                view_image = view.children("img"),
+                loading = $("#bubbl", view),
+                curr_image;
+            gallery.prepend(
+                '<div id="view"><span id="prev" class="control"></span><span id="close" class="control"></span><span id="next" class="control"></span><img><div id="bubbl"><span id="bubbl1"></span><span id="bubbl2"></span><span id="bubbl3"></span></div></div>'
+            );
+            function next() {
+                return curr_image.next().length ? curr_image.next() : curr_image.siblings("img").first();
+            };
+            function prev() {
+                return curr_image.prev("img").length ? curr_image.prev() : curr_image.nextAll().last();
+            };
+        
+            function viewImage(image) {
+                curr_image = image.data("full") ? image : next();
+                full_image = curr_image.data("full");
+                loading.show();
+                view_image.hide().attr('src', full_image).load(function() {
+                    loading.hide();
+                    view_image.show();
+                });
+            };
+        
+            gallery.click(function(e) {
+                var clicked = $(e.target)[0].tagName === "IMG" ? $(e.target) : null ;
+                if ( clicked ) {
+                    viewImage(clicked);  
+                    view.show();
+                }
+            });
+        
+            $( "#close", view ).click(function () { 
+                view.hide(); 
+            });
+            $( "#prev", view ).click(function () {
+                curr_image = prev();
+                viewImage(curr_image);
+            });
+            $( "#next", view ).click(function () {
+                curr_image = next();
+                viewImage(curr_image);
+            });
         });
-        view.find("#close").click(function () {
-            view.hide();
-        });
-        view.find("#prev").click(function () {
-            curr_image = curr_image.prev("img").length ?
-                curr_image.prev() : curr_image.nextAll().last();
-            viewImage();
-        });
-        view.find("#next").click(function () {
-            curr_image = curr_image.next().length ?
-                curr_image.next() : curr_image.siblings("img").first();
-            viewImage();
-        });
-    };
+    }        
 })(jQuery);
